120a132182ce4bb155af13bd1d54afe13afd66ad
matejstepan <matej.stepan@gmail.com>
2015-12-24 12:24:23 +0100

    test

---
+++ 24aktphrt.c
@@ -0,0 +1,52 @@
+
+
+#define CAS1 450
+#define CAS2 199
+#define CAS3 50
+
+
+task main(){
+    OnFwd(OUT_A+OUT_C);
+    OnFwd(OUT_B);
+    Wait(CAS1);
+    Off(OUT_A+OUT_C);
+    Wait(CAS3);
+    vlevoVbok();
+    Wait(CAS3);
+    PlaySound(1);
+    blik();
+    Wait(CAS3);
+    blik(); 
+    OnFwd(OUT_A+OUT_C);
+    OnFwd(OUT_B);
+    Wait(CAS1);
+    Off(OUT_A+OUT_C);
+    Wait(CAS3);
+    vlevoVbok();
+
+}
+void vlevoVbok(){
+	OnRev(OUT_A);
+	OnFwd(OUT_C);
+	Wait(CAS2);
+	Off(OUT_A+OUT_C);
+}
+
+void pravoVbok(){
+	OnFwd(OUT_A);
+	OnRev(OUT_C);
+	Wait(CAS2);
+	Off(OUT_A+OUT_C);
+}
+void  blik(){
+	repeat(3){
+		OnFwd(OUT_B);
+	   Wait(10);
+	    Off(OUT_B);
+	}	   
+}
+
+
+
+
+
--- klusrob.c
+++ klusrob.c
@@ -1,21 +1,14 @@
-#define SILA_A 7
-#define SILA_C 8
+#define SILA_A 8
+#define SILA_C 7
 
-//hodnoty pro baterie
-//#define CAS_VPRAVO_VBOK 430
-//#define CAS_VLEVO_VBOK 430
+#define CAS_VPRAVO_VBOK  190
+#define CAS_VLEVO_VBOK   190
 
-//hodnoty pro nabijecku
-#define CAS_VPRAVO_VBOK 190
-#define CAS_VLEVO_VBOK 190
-
-#define CAS_CUK 15
+#define CAS_CUK 11	
 #define CAS_DECIMETR 300
-
 task main(){
 	SetPower(OUT_A,SILA_A);
 	SetPower(OUT_C,SILA_C);
-
 	repeat(4){
 		jedRovneDecimetru(2);
 		PlaySound(4);
@@ -23,7 +16,7 @@ task main(){
 		pravoVbok();		
 	}	
 
-	PlaySound(1);
+	PlaySound(2);
 }
 
 void cukniLeva(){
@@ -38,7 +31,7 @@ void jedRovneDecimetru(int mamuject){
 	while (ujeljsem < mamuject){
 		OnFwd(OUT_A+OUT_C);
 		Wait(CAS_DECIMETR);
-		Off(OUT_A+OUT_C);
+		Float(OUT_A+OUT_C);
 		ujeljsem = ujeljsem + 1;
 		if (ujeljsem < mamuject) {
 			cukniLeva();
@@ -50,17 +43,20 @@ void jedRovneDecimetru(int mamuject){
 void couvej(int cas){
 	OnRev(OUT_A+OUT_C);
 	Wait(cas);
-	Off(OUT_A+OUT_C);
+	Float(OUT_A+OUT_C);
 }
+
 void pravoVbok(){
 	OnFwd(OUT_A);
 	OnRev(OUT_C);
 	Wait(CAS_VPRAVO_VBOK);
 	Off(OUT_A+OUT_C);
 }
+
 void vlevoVbok(){
 	OnRev(OUT_A);
 	OnFwd(OUT_C);
 	Wait(CAS_VLEVO_VBOK);
 	Off(OUT_A+OUT_C);
 }
+  
