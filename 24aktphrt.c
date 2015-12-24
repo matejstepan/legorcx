56db5a02ad51a977892b2aa501288528d95cb774
matejstepan <matej.stepan@gmail.com>
2015-12-24 21:12:23 +0100

    finale pro vanoce

--- 24aktphrt.c
+++ 24aktphrt.c
@@ -3,47 +3,62 @@
 #define CAS1 450
 #define CAS2 199
 #define CAS3 50
-
-
+#define CAS4 250
 task main(){
+  repeat(4){
+  	   OnFwd(OUT_A+OUT_C);
+  	   Wait(500);
+       cukniLeva();
+    	Off(OUT_A+OUT_C);
+   	 	Wait(CAS3);
+    	vlevoVbok();
+    	Wait(CAS3);
+    	PlaySound(1);
+    	blik(); 
+    	Wait(CAS3);
+    	pravoVbok();
+    	Wait(CAS2);
+    }
+    pravoVbok();
+    pravoVbok();
     OnFwd(OUT_A+OUT_C);
-    OnFwd(OUT_B);
-    Wait(CAS1);
-    Off(OUT_A+OUT_C);
-    Wait(CAS3);
-    vlevoVbok();
-    Wait(CAS3);
-    PlaySound(1);
+    Wait(500);
+    cukniLeva();
+    pravoVbok();
+ repeat(10){
+ 	PlaySound(4);
     blik();
     Wait(CAS3);
-    blik(); 
-    OnFwd(OUT_A+OUT_C);
-    OnFwd(OUT_B);
-    Wait(CAS1);
-    Off(OUT_A+OUT_C);
-    Wait(CAS3);
-    vlevoVbok();
+ }
+    
+}    
 
-}
 void vlevoVbok(){
 	OnRev(OUT_A);
 	OnFwd(OUT_C);
-	Wait(CAS2);
+	Wait(220);
 	Off(OUT_A+OUT_C);
 }
 
 void pravoVbok(){
 	OnFwd(OUT_A);
 	OnRev(OUT_C);
-	Wait(CAS2);
+	Wait(220);
 	Off(OUT_A+OUT_C);
 }
-void  blik(){
+
+void blik(){
 	repeat(3){
-		OnFwd(OUT_B);
+	   OnFwd(OUT_B);
 	   Wait(10);
-	    Off(OUT_B);
+	   Off(OUT_B);
 	}	   
+}
+void cukniLeva(){
+	OnRev(OUT_A);
+	OnFwd(OUT_C);
+	Wait(11);
+	Off(OUT_A+OUT_C);
 }
 
 
